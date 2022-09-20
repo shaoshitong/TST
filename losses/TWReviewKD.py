d8104d412fdfeb04350c170acee130e0d7d7a24a
shaoshitong <1090784053@qq.com>
2022-09-20 18:44:37 +0800

    reset to TWReviewkd

--- losses/TWReviewKD.py
+++ losses/TWReviewKD.py
@@ -120,18 +120,6 @@ class TWReviewKD(nn.Module):
     def hcl_loss(self,fstudent, fteacher):
         loss_all = 0.0
         for fs, ft in zip(fstudent, fteacher):
-            n, c, h, w = fs.shape
             loss = F.mse_loss(fs, ft, reduction="mean")
-            cnt = 1.0
-            tot = 1.0
-            for l in [4, 2, 1]:
-                if l >= h:
-                    continue
-                tmpfs = F.adaptive_avg_pool2d(fs, (l, l))
-                tmpft = F.adaptive_avg_pool2d(ft, (l, l))
-                cnt /= 2.0
-                loss += F.mse_loss(tmpfs, tmpft, reduction="mean") * cnt
-                tot += cnt
-            loss = loss / tot
-            loss_all = loss_all + loss
+            loss_all += loss
         return loss_all
