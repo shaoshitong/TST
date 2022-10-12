2bebb11de3c199559cfaa25a39abf0001569a871
shaoshitong <1090784053@qq.com>
2022-10-12 22:07:28 +0800

    add environment of Attack into LLACD

--- train_diversify.py
+++ train_diversify.py
@@ -17,7 +17,7 @@ from omegaconf import OmegaConf
 from torch.nn.parallel import DistributedDataParallel as DDP
 
 import wandb
-from Env.Environment_GAN import *
+from Env.Environment_Attack import *
 
 sys.path.append(os.path.join(os.getcwd()))
 import datas
